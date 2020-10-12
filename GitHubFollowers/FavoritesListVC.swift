d05c938387b9afb87db7acbedb7fd0a011d85e0e
vorontsov <vorontsov@nextexit.ru>
2020-10-12 22:57:05 +0300

    init project

--- GitHubFollowers/FavoritesListVC.swift
+++ GitHubFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHubFollowers
 //
 //  Created by ANDREY VORONTSOV on 12.10.2020.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by ANDREY VORONTSOV on 12.10.2020.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
