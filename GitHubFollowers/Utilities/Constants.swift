5e5241468d777dcbcb1ed37303b221edbb49766d
vorontsov <vorontsov@nextexit.ru>
2020-11-02 10:16:08 +0300

    User info view - final

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by ANDREY VORONTSOV on 02.11.2020.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
